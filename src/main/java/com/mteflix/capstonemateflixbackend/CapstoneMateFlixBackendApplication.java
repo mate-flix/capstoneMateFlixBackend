d06b14a31396889d529f4ec8257286907f48a05e
b.babatunde@native.semicolon.africa <you@example.com>
2023-11-24 07:44:01 +0100

    Initial commit

---
+++ src/main/java/com/mteflix/capstonemateflixbackend/CapstoneMateFlixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mteflix.capstonemateflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneMateFlixBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneMateFlixBackendApplication.class, args);
+    }
+
+}
