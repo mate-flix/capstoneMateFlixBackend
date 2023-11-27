81dc22f3dabc66b871ab7432765f7fd08c82bbc4
ViktorEmeadi <veetoreme@gmail.com>
2023-11-27 16:19:58 +0100

    My initial commit

---
+++ src/test/java/com/mteflix/capstonemateflixbackend/CapstoneMateFlixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mteflix.capstonemateflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneMateFlixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
