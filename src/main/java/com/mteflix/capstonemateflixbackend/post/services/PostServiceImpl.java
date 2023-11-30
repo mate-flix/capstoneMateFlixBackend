45d5497f4d0dbfd59e81eb0208c6622f3a2ee6f7
ViktorEmeadi <veetoreme@gmail.com>
2023-11-30 11:45:02 +0100

    updated

--- src/main/java/com/mteflix/capstonemateflixbackend/post/services/PostServiceImpl.java
+++ src/main/java/com/mteflix/capstonemateflixbackend/post/services/PostServiceImpl.java
@@ -1,13 +1,12 @@
 package com.mteflix.capstonemateflixbackend.post.services;
 
-import com.mteflix.capstonemateflixbackend.post.data.dto.request.Details;
+
 import com.mteflix.capstonemateflixbackend.post.data.dto.request.PostRequest;
 import com.mteflix.capstonemateflixbackend.post.data.dto.response.PostResponse;
 import com.mteflix.capstonemateflixbackend.post.data.model.Apartment;
 import com.mteflix.capstonemateflixbackend.post.data.model.Post;
-import com.mteflix.capstonemateflixbackend.post.data.model.User;
+import com.mteflix.capstonemateflixbackend.post.data.repository.ApartmentRepository;
 import com.mteflix.capstonemateflixbackend.post.data.repository.PostRepository;
-import com.mteflix.capstonemateflixbackend.post.data.repository.UserRepository;
 import com.mteflix.capstonemateflixbackend.post.exception.PostException;
 import com.mteflix.capstonemateflixbackend.post.utils.Utils;
 import lombok.RequiredArgsConstructor;
@@ -15,23 +14,49 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService{
-    private final UserRepository userRepository;
+    private final ApartmentRepository apartmentRepository;
+    private final CloudService cloudService;
+    private final PostRepository postRepository;
     private final Utils utils;
     @Override
     public PostResponse uploadPostWithPhoto(PostRequest postRequest) throws IOException {
-        Optional<User> userId = userRepository.findById(postRequest.getDetails().getUserId());
 
-        if (userId.isEmpty()){
-            throw new PostException("User with id "+postRequest.getDetails().getUserId()+" " +
-                    "not found");
-        }
-        Post post = utils.uploadWithPhoto(postRequest);
-        return new PostResponse("Uploaded");
+        utils.uploadWithPhoto(postRequest);
+        return new PostResponse("Apartment Uploaded");
     }
+
+    @Override
+    public PostResponse editApartment(PostRequest postRequest) throws IOException {
+        Optional<Apartment> foundApartment = apartmentRepository.findApartmentByHouseNumberAndId(
+                postRequest.getDetails().getHouseNumber(), postRequest.getDetails().getHouseId());
+        if (foundApartment.isEmpty()){
+            throw new PostException("Apartment with specified house number and ID does not exist");
+        }
+
+        Apartment apartment = foundApartment.get();
+        apartment.setDescription(postRequest.getDetails().getHouseDescription());
+        apartment.setHouseType(postRequest.getDetails().getHouseType());
+
+        Optional<Post> foundPost = postRepository.findById(postRequest.getDetails().getPostId());
+        if (foundPost.isEmpty()){
+            throw new PostException("Post with specified ID not found");
+        }
+        Post post = foundPost.get();
+        post.setApartment(apartment);
+        post.setTitle(postRequest.getDetails().getTitle());
+        post.setDescription(postRequest.getDetails().getMainDescription());
+        post.setDateUploaded(LocalDateTime.now());
+        if (postRequest.getDetails().getMultipartFile() != null) {
+            String newImageUrl = cloudService.upload(postRequest.getDetails().getMultipartFile());
+            post.setPhotoUrl(newImageUrl);
+        }
+        return new PostResponse("Apartment updated successfully");
+    }
+
 }
+
